4dca5ddb5e075f9d47fc0cf7a3c365893a887200
Aleksi Kaistinen <aleksi.kaistinen@allu.io>
2018-07-30 16:40:09 +0300

    Initial commit

---
+++ packages/setup.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env sh
+
+echo "$DIR"
+DIR=$(dirname "$0")
+cd "$DIR"
+echo "$DIR"
+
+. ../scripts/functions.sh
+
+COMMENT=\#*
+
+sudo -v
+
+info "Installing Brewfile packages..."
+brew bundle
+success "Finished installing Brewfile packages."
+
+find * -name "*.list" | while read fn; do
+    cmd="${fn%.*}"
+    set -- $cmd
+    info "Installing $1 packages..."
+    while read package; do
+        if [[ $package == $COMMENT ]];
+        then continue
+        fi
+        substep_info "Installing $package..."
+        $cmd $package
+    done < "$fn"
+    success "Finished installing $1 packages."
+done
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Allu's dotfiles
+
+```ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+git clone git@github.com:aleksik/dotfiles.git
+cd dotfiles
+./bootstrap.sh```
